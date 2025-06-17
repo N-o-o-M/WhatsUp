b79b0b323db963250319ffd02b3c04302cb28ea4
Mushaib <mushaibmo@gmail.com>
2025-06-17 12:27:07 +0530

    db connection established,login,sigin,signup done with api endpoints checked

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,89 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+
+  try {
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email: email,
+      fullName: fullName,
+      password: hashPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      const savedUser = await newUser.save();
+
+      res.status(201).json({
+        _id: savedUser._id,
+        email: savedUser.email,
+        fullName: savedUser.fullName,
+        profilePic: savedUser.profilePic,
+      });
+    } else {
+      return res.status(500).json({ message: "User creation failed" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+    const user = User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (req, res) => {
+  try {
+    res.cookie("token", "", {
+      maxAge: 0,
+    });
+  } catch (error) {
+    console.log("Error in logout controller:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,22 @@
 import express from "express";
-import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 
-app.listen(5001, () => {});
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log("Server is running on PORT:" + port);
+  connectDB()
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection failed:", error.message);
+    });
+});
