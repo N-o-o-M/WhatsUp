bc3df1fdd1a3c7305933c3e3b30f2cf1ab392852
Mushaib <mushaibmo@gmail.com>
2025-06-18 23:00:23 +0530

    edited frontend structure and added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,62 @@
-import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
-function App() {}
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore.js";
+
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+const App = () => {
+  const { theme } = useThemeStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    // Check authentication status on initial load
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="animate-spin w-10 h-10 text-blue-500" />
+      </div>
+    );
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+      <SignUpPage />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        ></Route>
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
 
 export default App;
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isLoggingOut: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.error("Error checking authentication:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  //   signup: async (data) => {},
+}));
